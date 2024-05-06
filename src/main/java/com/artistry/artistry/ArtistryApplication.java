0375ad121df9e8424503fb204b8b9ba61a246e69
zcbm7008 <zcbm7008@gmail.com>
2024-05-06 21:33:33 +0900

    Initial commit

---
+++ src/main/java/com/artistry/artistry/ArtistryApplication.java
@@ -0,0 +1,13 @@
+package com.artistry.artistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artistry/artistry/ArtistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artistry.artistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
