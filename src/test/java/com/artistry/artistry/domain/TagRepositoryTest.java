76a457af76b5991c1f68517d14401fb1f2a5248a
zcbm7008 <zcbm7008@gmail.com>
2024-05-08 21:28:33 +0900

    test : tag 더미 데이터 테스트

---
+++ src/test/java/com/artistry/artistry/domain/TagRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.artistry.artistry.domain;
+
+import com.artistry.artistry.Domain.Team;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class TagRepositoryTest {
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @DisplayName("태그 더미 데이터를 확인한다.")
+    @ParameterizedTest
+    @CsvSource({"1,tag1","2,tag2"})
+    void dummyTagTest(Long id,String name){
+        Optional<Tag> tag = tagRepository.findById(id);
+        assertThat(tag.isPresent()).isTrue();
+        assertThat(tag.get().getName()).isEqualTo(tagName);
+    }
+}
