57256d1a74f09919fbab2c64f476c1cb2455d11f
zcbm7008 <zcbm7008@gmail.com>
2024-05-08 21:19:41 +0900

    refactor : team test 리팩토링

--- src/test/java/com/artistry/artistry/domain/TeamRepositoryTest.java
+++ src/test/java/com/artistry/artistry/domain/TeamRepositoryTest.java
@@ -1,11 +1,8 @@
 package com.artistry.artistry.domain;
 
 import com.artistry.artistry.Domain.Team;
-import com.artistry.artistry.Exceptions.TeamNotFoundException;
 import com.artistry.artistry.Repository.TeamRepository;
-import com.artistry.artistry.Service.TeamService;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +14,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
-public class TeamTest {
+public class TeamRepositoryTest {
 
     @Autowired
     private TeamRepository teamRepository;
-
-    @Autowired
-    private TeamService teamService;
 
     @DisplayName("팀 더미데이터 확인")
     @ParameterizedTest
@@ -34,10 +28,5 @@ public class TeamTest {
         assertThat(team.get().getName()).isEqualTo(groupName);
     }
 
-    @DisplayName("팀 Id가 없을경우 예외를 던짐.")
-    @Test
-    void teamNotFound(){
-        assertThatThrownBy(() -> teamService.findById(Long.MAX_VALUE))
-                .isInstanceOf(TeamNotFoundException.class);
-    }
+
 }
---
+++ src/test/java/com/artistry/artistry/service/TeamServiceTest.java
@@ -0,0 +1,22 @@
+package com.artistry.artistry.service;
+
+import com.artistry.artistry.Exceptions.TeamNotFoundException;
+import com.artistry.artistry.Service.TeamService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+public class TeamServiceTest {
+    @Autowired
+    private TeamService teamService;
+    @DisplayName("팀 Id가 없을경우 예외를 던짐.")
+    @Test
+    void teamNotFound(){
+        assertThatThrownBy(() -> teamService.findById(Long.MAX_VALUE))
+                .isInstanceOf(TeamNotFoundException.class);
+    }
+}
