9e4e5019b367bd6d6f0022596b3bb3f3e055a477
zcbm7008 <zcbm7008@gmail.com>
2024-05-09 12:14:56 +0900

    feat : 역할 더미 구현 refactor : 더미 데이터 리펙토링

--- src/main/java/com/artistry/artistry/DataLoader.java
+++ src/main/java/com/artistry/artistry/DataLoader.java
@@ -1,8 +1,10 @@
 package com.artistry.artistry;
 
+import com.artistry.artistry.Domain.Role;
 import com.artistry.artistry.Domain.Tag;
 import com.artistry.artistry.Domain.Team;
 import com.artistry.artistry.Domain.Member;
+import com.artistry.artistry.Repository.RoleRepository;
 import com.artistry.artistry.Repository.TagRepository;
 import com.artistry.artistry.Repository.TeamRepository;
 import com.artistry.artistry.Repository.MemberRepository;
@@ -17,9 +19,17 @@ public class DataLoader implements CommandLineRunner {
     private final TeamRepository groupRepository;
     private final TagRepository tagRepository;
     private final MemberRepository memberRepository;
+    private final RoleRepository roleRepository;
 
     @Override
     public void run(String... args) throws Exception {
+        prepareDummyTeams();
+        prepareDummyTags();
+        prepareDummyMembers();
+        prepareDummyRoles();
+    }
+
+    private void prepareDummyTeams(){
         groupRepository.save(Team.builder()
                 .id(1L)
                 .name("Group1")
@@ -30,6 +40,10 @@ public class DataLoader implements CommandLineRunner {
                 .name("Group2")
                 .build());
 
+    }
+
+    private void prepareDummyTags(){
+
         tagRepository.save(Tag.builder()
                 .id(1L)
                 .name("band")
@@ -39,7 +53,9 @@ public class DataLoader implements CommandLineRunner {
                 .id(2L)
                 .name("edm")
                 .build());
+    }
 
+    private void prepareDummyMembers(){
         memberRepository.save(Member.builder()
                 .id(1L)
                 .nickname("member1")
@@ -49,7 +65,17 @@ public class DataLoader implements CommandLineRunner {
                 .id(2L)
                 .nickname("member2")
                 .build());
+    }
 
+    private void prepareDummyRoles(){
+        roleRepository.save(Role.builder()
+                .id(1L)
+                .roleName("vocal")
+                .build());
 
+        roleRepository.save(Role.builder()
+                .id(2L)
+                .roleName("composer")
+                .build());
     }
 }
--- src/test/java/com/artistry/artistry/domain/RoleRepositoryTest.java
+++ src/test/java/com/artistry/artistry/domain/RoleRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.artistry.artistry.domain;
 
+import com.artistry.artistry.Domain.Role;
+import com.artistry.artistry.Repository.RoleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class RoleRepositoryTest {
@@ -17,8 +21,10 @@ public class RoleRepositoryTest {
     @DisplayName("역할 더미 데이터를 확인한다.")
     @ParameterizedTest
     @CsvSource({"1,vocal","2,composer"})
-    void dummyRoleTest(Long id, String roleName){}
-    Optional<Role> role = roleRepository.findById(roleName);
-    assertThat(role.isPresent()).isTrue();
-    assertThat(role.get().getName()).isEqualTo(roleName);
+    void dummyRoleTest(Long id, String roleName){
+        Optional<Role> role = roleRepository.findById(id);
+        assertThat(role.isPresent()).isTrue();
+        assertThat(role.get().getRoleName()).isEqualTo(roleName);
+    }
+
 }
