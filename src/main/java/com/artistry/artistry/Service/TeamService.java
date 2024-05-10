2ff859db09259bd6c341550cfe3556b60e34d298
zcbm7008 <zcbm7008@gmail.com>
2024-05-10 12:36:42 +0900

    feat : 요청한 Id의 팀 반환 구현

--- src/main/java/com/artistry/artistry/Service/TeamService.java
+++ src/main/java/com/artistry/artistry/Service/TeamService.java
@@ -1,6 +1,8 @@
 package com.artistry.artistry.Service;
 
 import com.artistry.artistry.Domain.Team;
+import com.artistry.artistry.Dto.Response.TeamRequestDto;
+import com.artistry.artistry.Dto.Response.TeamResponseDto;
 import com.artistry.artistry.Exceptions.TeamNotFoundException;
 import com.artistry.artistry.Repository.TeamRepository;
 import org.springframework.stereotype.Service;
@@ -9,13 +11,29 @@ import org.springframework.stereotype.Service;
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    private final TagService tagService;
+    private final RoleService roleService;
+    private final MemberService memberService;
 
-    public TeamService(TeamRepository teamRepository){
+    public TeamService(TeamRepository teamRepository,TagService tagService,
+                       RoleService roleService, MemberService memberService){
         this.teamRepository = teamRepository;
+        this.tagService = tagService;
+        this.roleService = roleService;
+        this.memberService = memberService;
     }
 
-    public Team findById(Long id){
-        return teamRepository.findById(id)
-                .orElseThrow(TeamNotFoundException::new);
+    public TeamResponseDto findById(Long id){
+        return TeamResponseDto.from(teamRepository.findById(id)
+                .orElseThrow(TeamNotFoundException::new));
+    }
+
+    public TeamResponseDto create(TeamRequestDto teamRequestDto){
+        Team team = Team.builder()
+                .host(memberService.findById(teamRequestDto.getHostId()))
+                .roles(teamRequestDto.getRoles())
+                .tags(teamRequestDto.getTags())
+                .build();
+        return TeamResponseDto.from(teamRepository.save(team));
     }
 }
