da4ec6afa10bc522db18262855d2aa039f110d43
zcbm7008 <zcbm7008@gmail.com>
2024-05-10 13:05:19 +0900

    test : 포트폴리오 더미 테스트

---
+++ src/test/java/com/artistry/artistry/domain/PortfolioRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.artistry.artistry.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PortfolioRepositoryTest {
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+
+    @DisplayName("포트폴리오 더미 데이터를 확인한다")
+    @ParameterizedTest
+    @CsvSource({"1,Portfolio1","2,Portfolio2"})
+    void dummyPortfolioTest(Long id, String title){
+        Optional<Portfolio> portfolio = portfolioRepository.findById(id);
+        assertThat(portfolio.isPresent()).isTrue();
+        assertThat(portfolio.get().getNickname()).isEqualTo(title);
+
+    }
+
+
+}
