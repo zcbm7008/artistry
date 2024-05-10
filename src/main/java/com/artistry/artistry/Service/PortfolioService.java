e634a30733d8bf65fa88f639fc3900a6c41d14f6
zcbm7008 <zcbm7008@gmail.com>
2024-05-10 16:02:45 +0900

    fix : @Service추가

--- src/main/java/com/artistry/artistry/Service/PortfolioService.java
+++ src/main/java/com/artistry/artistry/Service/PortfolioService.java
@@ -3,8 +3,9 @@ package com.artistry.artistry.Service;
 import com.artistry.artistry.Domain.Portfolio;
 import com.artistry.artistry.Exceptions.RoleNotFoundException;
 import com.artistry.artistry.Repository.PortfolioRepository;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class PortfolioService {
     private final PortfolioRepository portfolioRepository;
 
