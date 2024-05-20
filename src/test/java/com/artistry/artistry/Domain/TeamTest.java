99d315a3ec854dc944ab8792ce25aa3a6707f778
zcbm7008 <zcbm7008@gmail.com>
2024-05-20 14:21:01 +0900

    refactor, test : 팀 application 테스트 생성

--- src/test/java/com/artistry/artistry/Domain/TeamTest.java
+++ src/test/java/com/artistry/artistry/Domain/TeamTest.java
@@ -1,6 +1,7 @@
 package com.artistry.artistry.Domain;
 
 import com.artistry.artistry.Exceptions.ArtistryDuplicatedException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,93 +11,75 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TeamTest {
 
-    @DisplayName("팀에 유저가 요청을 보낸다.")
-    @Test
-    void requestJoinTeam(){
-        //Given
-        Team team = createTeam();
-        Member applicant = Member.builder().nickname("지원자1").build();
-
-        //When
-        team.participate(applicant);
-
-        //Then
-        assertThat(team.getMembers()).hasSize(1);
-        assertThat(team.getMembers()).contains(applicant);
-
-    }
-
-    @DisplayName("같은 유저의 요청이 이미 있을경우 예외가 발생한다.")
-    @Test
-    void DuplicatedMemberException(){
-        //Given
-        Team team = createTeam();
-        Member applicant = Member.builder()
-                .nickname("지원자1").build();
-
-        //When
-        team.participate(applicant);
-
-        //Then
-        assertThatThrownBy(() -> team.participate(applicant)).isInstanceOf(ArtistryDuplicatedException.class);
-
-    }
-
-    @DisplayName("한 포지션에 같은 멤버의 포트폴리오가 여러개면 중복처리한다.")
-    @Test
-    void DuplicatedRolePortfolioException(){
-        //Given
-        Role role1 = Role.builder().roleName("작곡가").build();
-
-        List<Portfolio> portfolios = new ArrayList<>();
-
-        Member applicant = Member.builder()
-                .nickname("지원자1").portfolios(portfolios).build();
-
-        Portfolio portfolio1 = Portfolio.builder()
-                .role(role1).member(applicant).title("portfolio1").build();
-
-
-        Portfolio portfolio2 = Portfolio.builder()
-                .role(role1).member(applicant).title("portfolio2").build();
-
-        applicant.addPortfolio(portfolio1);
-        applicant.addPortfolio(portfolio2);
-
-        Team team = createTeam();
-
-        //when
-        team.apply(applicant.getPortfolios().get(0));
-
-        //then
-        assertThatThrownBy(() -> team.apply(applicant.getPortfolios().get(1))).isInstanceOf(ArtistryDuplicatedException.class);
-    }
-
-    private Team createTeam(){
-        List<Tag> tags= Arrays.asList(
-                Tag.builder().name("밴드").build(),
-                Tag.builder().name("얼터너티브 락").build()
+    private Team team;
+    private List<Tag> tags;
+    private List<Role> roles;
+    private Member host;
+    private Member member;
+    private Portfolio portfolio;
+    private Application application;
+    private Role appliedRole;
+    @BeforeEach
+    public void setUp(){
+        tags= Arrays.asList(
+                Tag.of("밴드"),
+                Tag.of("락")
         );
 
-        List<Role> roles = Arrays.asList(
-                Role.builder().roleName("작곡가").build(),
-                Role.builder().roleName("보컬").build()
+        roles = Arrays.asList(
+                Role.of("작곡가"),
+                Role.of("보컬")
         );
 
-        Member host = Member.builder()
+        host = Member.builder()
                 .nickname("호스트")
                 .build();
 
-        return Team.builder()
+        member = Member.builder()
+                .nickname("멤버1")
+                .build();
+
+        team = Team.builder()
                 .name("밴드")
                 .tags(tags)
                 .host(host)
-                .roles(roles)
                 .build();
+
+        team.addRoles(roles);
+
+        appliedRole = roles.get(0);
+
+        application = Application.builder()
+                .team(team)
+                .role(appliedRole)
+                .member(member)
+                .status(ApplicationStatus.PENDING)
+                .build();
+
     }
+
+    @Test
+    @DisplayName("팀의 특정한 역할에 지원서를 지원한다.")
+    public void applyToTeam(){
+
+        team.apply(team.getTeamRoles().get(0), application);
+        assertEquals(application.getId(),team.getTeamRoles().get(0).getApplications().get(0).getId());
+        assertEquals(application.getTeam().getId(),team.getId());
+        assertEquals(application.getRole(),team.findTeamRoleByRole(appliedRole).getRole());
+
+    }
+    @Test
+    @DisplayName("지원한 application의 상태는 PENDING이어야 한다.")
+    public void isStatusPending(){
+        team.apply(team.getTeamRoles().get(0), application);
+        assertEquals(application.getStatus(),ApplicationStatus.PENDING);
+
+    }
+
 
 
 }
