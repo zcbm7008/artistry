81b48492dbdcd62fc090f406966b38bb17dad0d0
zcbm7008 <zcbm7008@gmail.com>
2024-05-21 16:33:56 +0900

    feat : DocsController test : TeamRoleRepositoryTest

---
+++ src/test/java/com/artistry/artistry/Domain/Repository/TeamRoleRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.artistry.artistry.Domain.Repository;
+
+import com.artistry.artistry.Repository.TeamRoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TeamRoleRepositoryTest {
+    @Autowired
+    TeamRoleRepository teamRoleRepository;
+
+}
