6dff2e59ddcdc28f267704f34e653fb1f68cf5a6
zcbm7008 <zcbm7008@gmail.com>
2024-05-21 13:43:58 +0900

    refactor : TeamRepositoryTest 리팩토링 //Todo : 팀 더미데이터 확인

--- src/test/java/com/artistry/artistry/Domain/Repository/TeamRepositoryTest.java
+++ src/test/java/com/artistry/artistry/Domain/Repository/TeamRepositoryTest.java
@@ -1,10 +1,14 @@
 package com.artistry.artistry.Domain.Repository;
 
 
-import com.artistry.artistry.Domain.Member;
-import com.artistry.artistry.Domain.Role;
-import com.artistry.artistry.Domain.Tag;
-import com.artistry.artistry.Domain.Team;
+import com.artistry.artistry.Domain.*;
+import com.artistry.artistry.Dto.Request.RoleRequest;
+import com.artistry.artistry.Dto.Request.TagRequest;
+import com.artistry.artistry.Dto.Request.TeamRequest;
+import com.artistry.artistry.Dto.Response.TeamResponse;
+import com.artistry.artistry.Repository.MemberRepository;
+import com.artistry.artistry.Repository.RoleRepository;
+import com.artistry.artistry.Repository.TagRepository;
 import com.artistry.artistry.Repository.TeamRepository;
 import com.artistry.artistry.Service.MemberService;
 import com.artistry.artistry.Service.RoleService;
@@ -19,6 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,51 +38,55 @@ public class TeamRepositoryTest {
     private RoleService roleService;
     @Autowired
     private MemberService memberService;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
-    @DisplayName("팀 더미데이터 확인")
-    @ParameterizedTest
-    @CsvSource({"1,team1","2,team2"})
-    void dummyGroupTest(Long id, String groupName){
-        Optional<Team> team = teamRepository.findById(id);
-        assertThat(team.isPresent()).isTrue();
-        assertThat(team.get().getName()).isEqualTo(groupName);
-    }
+//    @DisplayName("팀 더미데이터 확인")
+//    @ParameterizedTest
+//    @CsvSource({"1,team1","2,team2"})
+//    void dummyGroupTest(Long id, String groupName){
+//        Optional<Team> team = teamRepository.findById(id);
+//        assertThat(team.isPresent()).isTrue();
+//        assertThat(team.get().getName()).isEqualTo(groupName);
+//    }
 
-    @DisplayName("생성한 팀을 저장한다")
+    @DisplayName("팀을 생성한다")
     @Test
-    void saveTest() {
-        Team team = saveTeam();
-        teamRepository.flush();
+    void createTeam(){
+        String teamName = "밴드 팀";
+        String roleName1 = "작곡가";
+        String roleName2 = "드럼";
+        String tagName1 = "밴드";
+        String tagName2 = "락";
+        Member member1 = memberRepository.save(new Member("member1"));
+        List <Role> roles = Arrays.asList( roleRepository.save(new Role(roleName1)), roleRepository.save(new Role(roleName2)));
+        List <Tag> tags = Arrays.asList(tagRepository.save(new Tag(tagName1)),tagRepository.save(new Tag(tagName2)));
 
-        assertThat(teamRepository.existsById(team.getId())).isTrue();
+        Team team = teamRepository.save(new Team(teamName,member1,tags,roles));
 
+        assertThat(team.getId()).isNotNull();
+        assertThat(team.getTags()).containsExactlyInAnyOrderElementsOf(tags);
+        assertThat(team.getTeamRoles().stream().map(TeamRole::getRole).collect(Collectors.toList())).containsExactlyInAnyOrderElementsOf(roles);
 
-    }
-
-    private Team saveTeam(){
-        List<Role> roles = Arrays.asList(roleService.findById(1L),
-                roleService.findById(2L));
-
-        List<Member> members = Arrays.asList(memberService.findById(1L),
-                memberService.findById(2L));
-
-        List<Tag> tags = Arrays.asList(tagService.findById(1L),
-                tagService.findById(2L));
-
-        return teamRepository.save(Team.builder()
-                .name("team1")
-                .host(memberService.findById(1L))
-                .roles(roles)
-                .members(members)
-                .tags(tags)
-                .build());
     }
 
     @DisplayName("팀 생성시각을 저장한다.")
     @Test
     void saveTeamCreatedDate() {
-        Team team = saveTeam();
+        String teamName = "밴드 팀";
+        String roleName1 = "작곡가";
+        String roleName2 = "드럼";
+        String tagName1 = "밴드";
+        String tagName2 = "락";
+        Member member1 = memberRepository.save(new Member("member1"));
+        List <Role> roles = Arrays.asList( roleRepository.save(new Role(roleName1)), roleRepository.save(new Role(roleName2)));
+        List <Tag> tags = Arrays.asList(tagRepository.save(new Tag(tagName1)),tagRepository.save(new Tag(tagName2)));
 
+        Team team = teamRepository.save(new Team(teamName,member1,tags,roles));
         assertThat(team.getCreatedAt()).isNotNull();
     }
 
