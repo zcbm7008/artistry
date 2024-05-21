94b68d1d50077257f9166ef248eb4b927c20170e
zcbm7008 <zcbm7008@gmail.com>
2024-05-21 18:52:53 +0900

    feat : .Dockerfile

--- src/test/java/com/artistry/artistry/restdocs/TeamApiDocTest.java
+++ src/test/java/com/artistry/artistry/restdocs/TeamApiDocTest.java
@@ -21,6 +21,7 @@ import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
@@ -39,6 +40,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
 public class TeamApiDocTest {
@@ -54,6 +56,7 @@ public class TeamApiDocTest {
     TeamRepository teamRepository;
     @Autowired
     MemberRepository memberRepository;
+    private Team dummyTeam;
 
     @BeforeEach
     public void setUp(WebApplicationContext webApplicationContext,
@@ -76,9 +79,8 @@ public class TeamApiDocTest {
         String dummyTeamName = "더미 팀";
         List<Role> roles = Arrays.asList(role1, role2);
         List<Tag> tags = Arrays.asList(tag1, tag2);
-        Team dummyTeam = new Team(dummyTeamName, member1, tags, roles);
+        dummyTeam = new Team(dummyTeamName, member1, tags, roles);
         teamRepository.save(dummyTeam);
-
 
     }
 
@@ -141,7 +143,7 @@ public class TeamApiDocTest {
     @DisplayName("팀을 조회한다")
     @Test
     void readTeamTest() throws Exception{
-        mockMvc.perform(get("/api/teams/1")
+        mockMvc.perform(get("/api/teams/" + dummyTeam.getId())
                 .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.teamId").exists())
@@ -150,8 +152,8 @@ public class TeamApiDocTest {
                 .andExpect(jsonPath("$.host.nickName").exists())
                 .andExpect(jsonPath("$.tags").isArray())
                 .andExpect(jsonPath("$.tags",hasSize(2)))
-                .andExpect(jsonPath("$.tags",hasItem("밴드")))
-                .andExpect(jsonPath("$.tags",hasItem("락")))
+                .andExpect(jsonPath("$.tags",hasItem("band")))
+                .andExpect(jsonPath("$.tags",hasItem("edm")))
                 .andExpect(jsonPath("$.teamRoles").exists())
                 .andExpect(jsonPath("$.teamRoles",hasSize(2)))
                 .andDo(document("read-room",
