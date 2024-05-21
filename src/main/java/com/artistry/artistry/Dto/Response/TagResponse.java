024e5ed310d08becfa0b312009c83b9c27115eba
zcbm7008 <zcbm7008@gmail.com>
2024-05-21 16:32:46 +0900

    feat : TagResponse 생성, TagService findAllById

---
+++ src/main/java/com/artistry/artistry/Dto/Response/TagResponse.java
@@ -0,0 +1,20 @@
+package com.artistry.artistry.Dto.Response;
+
+import com.artistry.artistry.Domain.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagResponse {
+    private Long id;
+    private String name;
+    public static TagResponse from(Tag tag){
+        return new TagResponse(
+                tag.getId(),
+                tag.getName()
+        );
+    }
+}
--- src/main/java/com/artistry/artistry/Service/TagService.java
+++ src/main/java/com/artistry/artistry/Service/TagService.java
@@ -1,9 +1,13 @@
 package com.artistry.artistry.Service;
 
 import com.artistry.artistry.Domain.Tag;
+import com.artistry.artistry.Dto.Request.TagRequest;
 import com.artistry.artistry.Exceptions.TagNotFoundException;
 import com.artistry.artistry.Repository.TagRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TagService {
@@ -17,5 +21,11 @@ public class TagService {
     public Tag findById(Long id){
         return tagRepository.findById(id)
                 .orElseThrow(TagNotFoundException::new);
+    }
+
+    public List<Tag> findAllById(final List<TagRequest> tagRequests){
+        return tagRequests.stream()
+                .map(tagRequest -> findById(tagRequest.getId()))
+                .collect(Collectors.toList());
     }
 }
