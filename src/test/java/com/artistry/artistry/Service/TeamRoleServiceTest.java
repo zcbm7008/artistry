5ab3450cb8a4711c2b5d36163346e3f5806e3e12
zcbm7008 <zcbm7008@gmail.com>
2024-05-21 11:36:39 +0900

    test : TeamRole id 예외 테스트 추가

--- src/test/java/com/artistry/artistry/Service/TeamRoleServiceTest.java
+++ src/test/java/com/artistry/artistry/Service/TeamRoleServiceTest.java
@@ -3,7 +3,8 @@ package com.artistry.artistry.Service;
 import com.artistry.artistry.Domain.*;
 import com.artistry.artistry.Dto.Response.ApplicationResponse;
 import com.artistry.artistry.Dto.Response.PortfolioResponse;
-import com.artistry.artistry.Dto.Response.TeamResponse;
+import com.artistry.artistry.Exceptions.TeamNotFoundException;
+import com.artistry.artistry.Exceptions.TeamRoleNotFoundException;
 import com.artistry.artistry.Repository.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class TeamRoleServiceTest {
@@ -42,32 +44,49 @@ public class TeamRoleServiceTest {
         String tagName2 = "락";
         Member member1 = memberRepository.save(new Member("member1"));
         Member member2 = memberRepository.save(new Member("member2"));
-        Member applicant = memberRepository.save(new Member("applicant"));
+        Member applicant1 = memberRepository.save(new Member("applicant1"));
+        Member applicant2 = memberRepository.save(new Member("applicant2"));
         Role role1 = roleRepository.save(new Role(roleName1));
         Role role2 = roleRepository.save(new Role(roleName2));
         Tag tag1 = tagRepository.save(new Tag(tagName1));
         Tag tag2 = tagRepository.save(new Tag(tagName2));
 
         Portfolio portfolio1 = portfolioRepository.save(new Portfolio(1L, "portfolio1 for composer", role1, member2));
-        Portfolio portfolio2 = portfolioRepository.save(new Portfolio(2L, "portfolio2 for drummer", role2, applicant));
+        Portfolio portfolio2 = portfolioRepository.save(new Portfolio(2L, "portfolio2 for drummer", role2, applicant1));
+        Portfolio portfolio3 = portfolioRepository.save(new Portfolio(3L, "portfolio3 for drummer", role2, applicant2));
 
         Team team = new Team(teamName, member1, Arrays.asList(tag1, tag2), Arrays.asList(role1, role2));
 
         teamRepository.save(team);
 
         Application application1 = applicationRepository.save(new Application(team, role1, team.findTeamRoleByRole(role1), member2, portfolio1));
-        Application application2 = applicationRepository.save(new Application(team, role2, team.findTeamRoleByRole(role2), applicant, portfolio2));
+        Application application2 = applicationRepository.save(new Application(team, role2, team.findTeamRoleByRole(role2), applicant1, portfolio2));
+        Application application3 = applicationRepository.save(new Application(team, role2, team.findTeamRoleByRole(role2), applicant2, portfolio3));
 
-        team.addApplicationInTeamRole(role1, application1);
+        team.findTeamRoleByRole(role1).addApplication(application1);
         team.findTeamRoleByRole(role2).getApplications().add(application2);
+        team.findTeamRoleByRole(role2).getApplications().add(application3);
 
         teamRepository.save(team);
 
-        List<ApplicationResponse> foundApplications = teamRoleService.getApplications(team.findTeamRoleByRole(role1).getId());
+        List<ApplicationResponse> role1Applications = teamRoleService.getApplications(team.findTeamRoleByRole(role1).getId());
+        List<ApplicationResponse> role2Applications = teamRoleService.getApplications(team.findTeamRoleByRole(role2).getId());
 
-        assertThat(foundApplications)
+        assertThat(role1Applications)
                 .extracting(ApplicationResponse::getPortfolio)
                 .map(PortfolioResponse::getId)
                 .containsExactly(portfolio1.getId());
+
+        assertThat(role2Applications)
+                .extracting(ApplicationResponse::getPortfolio)
+                .map(PortfolioResponse::getId)
+                .containsExactly(portfolio2.getId(),portfolio3.getId());
+    }
+
+    @DisplayName("요청한 팀 Id가 없을경우 예외를 던짐.")
+    @Test
+    void teamNotFound(){
+        assertThatThrownBy(() -> teamRoleService.findById(Long.MAX_VALUE))
+                .isInstanceOf(TeamRoleNotFoundException.class);
     }
 }
