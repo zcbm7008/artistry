2912e4c1544769dd3bf2e523850b9106f1181c3a
zcbm7008 <zcbm7008@gmail.com>
2024-05-24 14:25:25 +0900

    feat : 태그 단일 조회 api 구현

--- src/test/java/com/artistry/artistry/restdocs/TagApiDocTest.java
+++ src/test/java/com/artistry/artistry/restdocs/TagApiDocTest.java
@@ -3,18 +3,22 @@ package com.artistry.artistry.restdocs;
 
 import com.artistry.artistry.Domain.tag.Tag;
 import com.artistry.artistry.Dto.Request.TagCreateRequest;
+import com.artistry.artistry.Dto.Request.TagRequest;
 import com.artistry.artistry.Dto.Response.TagResponse;
 import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,13 +30,30 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 public class TagApiDocTest extends ApiTest{
+    private List<TagResponse> createTags = new ArrayList<>();
 
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation){
+        super.setUp(restDocumentation);
+
+        createTags.add(tagSave("트랩"));
+        createTags.add(tagSave("퓨처베이스"));
+
+    }
+
+    public static TagResponse tagSave(final String tagName){
+        Map<String, Object> body = new HashMap<>();
+        body.put("name",tagName);
+
+        return given().body(body)
+                .when().post("/api/tags")
+                .then().statusCode(HttpStatus.OK.value())
+                .extract().body().as(TagResponse.class);
+    }
 
     @DisplayName("전체 태그를 가져온다.")
     @Test
     void getAllTagsTest() {
-        tagRepository.save(new Tag("태그1"));
-        tagRepository.save(new Tag("태그2"));
 
         List<TagResponse> responses =
                 given()
@@ -47,6 +68,28 @@ public class TagApiDocTest extends ApiTest{
 
         assertThat(responses).isNotEmpty();
     }
+
+    @DisplayName("태그 단일 조회")
+    @Test
+    void getTag() {
+        Long tagId = createTags.get(0).getId();
+
+        TagResponse response =
+                given()
+                        .filter(RestAssuredRestDocumentationWrapper.document("create-tag",
+                                "태그 단일 조회 API",
+                                responseFields(
+                                        fieldWithPath("id").description("태그 Id"),
+                                        fieldWithPath("name").description("태그 이름"))))
+                        .when().get("/api/tags/{id}", tagId)
+                        .then().statusCode(HttpStatus.OK.value())
+                        .extract().body().as(TagResponse.class);
+
+        TagResponse expectedTag = createTags.get(0);
+        assertThat(response.getName()).isEqualTo(expectedTag.getName());
+    }
+
+
     @DisplayName("태그를 생성한다.")
     @Test
     void createTag(){
