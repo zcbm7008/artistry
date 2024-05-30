567a3bbb375c13b14be28ff1c40d6499b0668a84
zcbm7008 <zcbm7008@gmail.com>
2024-05-30 13:19:22 +0900

    test : 멤버 findByEmail 테스트 추가

--- src/test/java/com/artistry/artistry/Domain/Repository/MemberRepositoryTest.java
+++ src/test/java/com/artistry/artistry/Domain/Repository/MemberRepositoryTest.java
@@ -1,8 +1,13 @@
 package com.artistry.artistry.Domain.Repository;
 
+import com.artistry.artistry.Domain.member.Member;
 import com.artistry.artistry.Repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class MemberRepositoryTest {
@@ -10,5 +15,14 @@ public class MemberRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @DisplayName("특정 이메일을 가진 Member를 조회한다.")
+    @Test
+    void findMemberByEmail(){
+        String expectedEmail = "a@a.com";
+        Member member1 = memberRepository.save(new Member("n1",expectedEmail));
+        Member foundMember = memberRepository.findByEmail(member1.getEmail());
+
+        assertThat(expectedEmail).isEqualTo(foundMember.getEmail());
+    }
 
 }
