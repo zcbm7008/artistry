7ead074d72732b941de9029c7fdf3bfdcec0dbcd
zcbm7008 <zcbm7008@gmail.com>
2024-06-03 23:17:30 +0900

    feat : mvcConfig, ArgumentResolver, Interceptor 구현

---
+++ src/main/java/com/artistry/artistry/auth/AuthInterceptor.java
@@ -0,0 +1,31 @@
+package com.artistry.artistry.auth;
+
+import com.artistry.artistry.Exceptions.ArtistryInvalidValueException;
+import com.artistry.artistry.Exceptions.MemberNotFoundException;
+import com.artistry.artistry.Repository.MemberRepository;
+import com.artistry.artistry.auth.jwt.JwtTokenProvider;
+import com.artistry.artistry.auth.jwt.JwtTokenExtractor;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthContext authContext;
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null) {
+            throw new ArtistryInvalidValueException("인증 정보가 없습니다.");
+        }
+        Long memberId = jwtTokenProvider.parseId(authHeader);
+        authContext.setMemberId(memberId);
+        return true;
+    }
+}
---
+++ src/main/java/com/artistry/artistry/config/WebMvcConfiguration.java
@@ -0,0 +1,33 @@
+package com.artistry.artistry.config;
+
+import com.artistry.artistry.auth.AuthInterceptor;
+import com.artistry.artistry.auth.MemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private final MemberArgumentResolver memberArgumentResolver;
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry){
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/auth/**")
+                .excludePathPatterns("/api/members/**")
+                .excludePathPatterns("/api/teams");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberArgumentResolver);
+    }
+}
