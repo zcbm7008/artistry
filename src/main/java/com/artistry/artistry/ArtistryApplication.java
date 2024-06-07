7877d626211b88c08b592b9d455c56de7a381ce5
zcbm7008 <zcbm7008@gmail.com>
2024-06-08 02:24:21 +0900

    feat : 테스트를 위한 application 수정

--- src/main/java/com/artistry/artistry/ArtistryApplication.java
+++ src/main/java/com/artistry/artistry/ArtistryApplication.java
@@ -2,14 +2,19 @@ package com.artistry.artistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class ArtistryApplication {
-
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.properties,"
+			+ "classpath:real-application.yml";
 	public static void main(String[] args) {
-		SpringApplication.run(ArtistryApplication.class, args);
+		new SpringApplicationBuilder(ArtistryApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
