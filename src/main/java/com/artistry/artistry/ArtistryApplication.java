c086857abf87b3b910aea66ad7544c4ced1638e8
zcbm7008 <zcbm7008@gmail.com>
2024-06-08 21:24:37 +0900

    fix : build시 jar파일 수정

--- src/main/java/com/artistry/artistry/ArtistryApplication.java
+++ src/main/java/com/artistry/artistry/ArtistryApplication.java
@@ -8,13 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class ArtistryApplication {
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.properties,"
-			+ "classpath:real-application.yml";
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ArtistryApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(ArtistryApplication.class, args);
 	}
 
 }
