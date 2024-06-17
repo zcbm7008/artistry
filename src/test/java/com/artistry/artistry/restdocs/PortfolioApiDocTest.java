60ba5dddfde5ad95a39300763e95eb91ebec8c00
zcbm7008 <zcbm7008@gmail.com>
2024-06-17 19:52:23 +0900

    test : api 페이지네이션 테스트 구현

--- src/test/java/com/artistry/artistry/restdocs/PortfolioApiDocTest.java
+++ src/test/java/com/artistry/artistry/restdocs/PortfolioApiDocTest.java
@@ -8,6 +8,7 @@ import com.artistry.artistry.Dto.Request.LinkRequest;
 import com.artistry.artistry.Dto.Request.RoleRequest;
 import com.artistry.artistry.Dto.Response.PortfolioResponse;
 import com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper;
+import io.restassured.response.Response;
 import lombok.NonNull;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,27 +41,28 @@ public class PortfolioApiDocTest extends ApiTest{
         Map<String, Object> body = getStringObjectMap(role1, title,"PUBLIC");
         body.put("memberId",member1.getId());
 
-        PortfolioResponse response =
-                given().body(body)
-                        .when().post("/api/portfolios")
-                        .then().statusCode(HttpStatus.OK.value())
-                        .extract().body().as(PortfolioResponse.class);
+        PortfolioResponse response = getPortfolioResponse(given().body(body)
+                .when().post("/api/portfolios"));
 
         createdPortfolios.add(response);
 
         String title2 = "보컬 포트폴리오1";
 
         Role role2 = roleRepository.save(new Role("보컬"));
-        Map<String, Object> body2 = getStringObjectMap(role1, title,"PRIVATE");
+        Map<String, Object> body2 = getStringObjectMap(role1, title2,"PRIVATE");
         body2.put("memberId",member1.getId());
 
-        PortfolioResponse response2 =
-                given().body(body2)
-                        .when().post("/api/portfolios")
-                        .then().statusCode(HttpStatus.OK.value())
-                        .extract().body().as(PortfolioResponse.class);
+        PortfolioResponse response2 = getPortfolioResponse(given().body(body2)
+                .when().post("/api/portfolios"));
 
         createdPortfolios.add(response2);
+    }
+
+    private static PortfolioResponse getPortfolioResponse(Response body2) {
+        PortfolioResponse response2 = body2
+                        .then().statusCode(HttpStatus.OK.value())
+                        .extract().body().as(PortfolioResponse.class);
+        return response2;
     }
 
 
@@ -117,7 +119,6 @@ public class PortfolioApiDocTest extends ApiTest{
         assertThat(response.getAccess()).isEqualTo("PUBLIC");
     }
 
-
     @DisplayName("전체 포트폴리오를 가져온다.")
     @Test
     void getAllPortfolios() {
@@ -149,6 +150,41 @@ public class PortfolioApiDocTest extends ApiTest{
                         .extract().body().jsonPath().getList(".", PortfolioResponse.class);
 
         assertThat(responses).isNotNull();
+    }
+
+
+    @DisplayName("전체 포트폴리오를 페이지네이션으로 가져온다.")
+    @Test
+    void getAllPagePortfolios() {
+        List<PortfolioResponse> responses =
+                given()
+                        .filter(RestAssuredRestDocumentationWrapper.document("read-portfolios",
+                                "모든 포트폴리오 조회 API",
+                                responseFields(
+                                        fieldWithPath("[].id").description("포트폴리오 Id"),
+                                        fieldWithPath("[].roleName").description("포트폴리오 역할"),
+                                        fieldWithPath("[].title").description("포트폴리오 제목"),
+                                        fieldWithPath("[].view").description("포트폴리오 조회수"),
+                                        fieldWithPath("[].like").description("포트폴리오 like"),
+                                        fieldWithPath("[].member").description("포트폴리오 소유 멤버"),
+                                        fieldWithPath("[].member.id").description("멤버 id"),
+                                        fieldWithPath("[].member.nickName").description("멤버 닉네임"),
+                                        fieldWithPath("[].member.email").description("멤버 이메일"),
+                                        fieldWithPath("[].member.iconUrl").description("멤버 아이콘 Url"),
+                                        fieldWithPath("[].member.bio").description("멤버 소개"),
+                                        fieldWithPath("[].member.links").description("멤버 링크"),
+                                        fieldWithPath("[].member.links[].url").description("멤버 링크 url"),
+                                        fieldWithPath("[].member.links[].comment").description("멤버 링크 코멘트"),
+                                        fieldWithPath("[].contents").description("포트폴리오 컨텐츠들"),
+                                        fieldWithPath("[].contents[].url").description("컨텐츠 url"),
+                                        fieldWithPath("[].contents[].comment").description("컨텐츠 설명"),
+                                        fieldWithPath("[].access").description("포트폴리오 접근권한"))))
+                        .when().get("/api/portfolios?page=1&size=1")
+                        .then().statusCode(HttpStatus.OK.value())
+                        .extract().body().jsonPath().getList(".", PortfolioResponse.class);
+
+        assertThat(responses).isNotNull();
+        assertThat(responses).hasSize(1);
     }
 
     @DisplayName("Id로 포트폴리오를 조회")
