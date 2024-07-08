5ed1f6e8572dca78d1fb4479c367fecee2f895eb
zcbm7008 <zcbm7008@gmail.com>
2024-07-08 13:17:49 +0900

    feat : caffeine 종속성 추가 및 caching 활성화

--- src/main/java/com/artistry/artistry/ArtistryApplication.java
+++ src/main/java/com/artistry/artistry/ArtistryApplication.java
@@ -3,10 +3,12 @@ package com.artistry.artistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class ArtistryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ArtistryApplication.class, args);
