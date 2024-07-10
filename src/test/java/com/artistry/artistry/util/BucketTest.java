7e8b57e64b5784c4b978a13ca281d864fdde66bb
zcbm7008 <zcbm7008@gmail.com>
2024-07-10 21:05:24 +0900

    chore : bucket4j 테스트 log 설정

--- src/test/java/com/artistry/artistry/util/BucketTest.java
+++ src/test/java/com/artistry/artistry/util/BucketTest.java
@@ -25,11 +25,14 @@ public class BucketTest extends ApiTest {
 
     @Test
     void testBucket4j() throws InterruptedException {
+        System.out.println("Initial tokens: " + bucket.getAvailableTokens());
         bucket.tryConsumeAsMuchAsPossible();
+        System.out.println("Tokens after tryConsumeAsMuchAsPossible: " + bucket.getAvailableTokens());
         bucket.tryConsume(25);
+        System.out.println("Tokens after tryConsume(25): " + bucket.getAvailableTokens());
+
         given().when().get("/api/tags")
                 .then()
                 .statusCode(HttpStatus.TOO_MANY_REQUESTS.value());
-
     }
 }
